d70398f318fd0032afa70f1ba79f223cf21fdd63
Sujay <kumarsujay21@gmail.com>
2018-02-16 17:55:21 +0530

    android maven dependency added

--- app/src/main/java/com/wildox/logger/MainActivity.java
+++ app/src/main/java/com/wildox/logger/MainActivity.java
@@ -2,14 +2,9 @@ package com.wildox.logger;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.wildox.loghere.LogHere;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,26 +20,18 @@ public class MainActivity extends AppCompatActivity {
          * printing the logs if it's in release mode
           */
         LogHere.setDebugMode(BuildConfig.DEBUG);
-        LogHere.setPackageName(getPackageName());
+        //LogHere.setPackageName(getPackageName());
 
         FloatingActionButton seeLogs = (FloatingActionButton) findViewById(R.id.see_logs);
         seeLogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LogHere.e("anything");
                 thisIsMe();
             }
         });
     }
 
     void thisIsMe() {
-        LogHere.e("zero parent", 0);
-
-        LogHere.e("one parent", 1);
-        thisIsNotMe();
-    }
-
-    void thisIsNotMe() {
-        LogHere.e("two parent", 2);
+        //thisIsNotMe();
     }
 }
